3b62cc0376d8fb9d3533d93e4fb6e76d73db9994
Rafael Fonseca Hermes Azevedo <rafael.azevedo.dev@gmail.com>
2025-04-07 23:03:18 -0300

    adicionado swagger ao projeto

--- src/main/java/br/com/digimon/infra/controller/UsuarioController.java
+++ src/main/java/br/com/digimon/infra/controller/UsuarioController.java
@@ -4,6 +4,9 @@ import br.com.digimon.app.dto.CriarUsuarioDTO;
 import br.com.digimon.app.dto.RespostaPadraoDTO;
 import br.com.digimon.app.usecase.UsuarioUseCase;
 import br.com.digimon.domain.port.in.Usuario;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -21,12 +24,22 @@ public class UsuarioController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    //@Operation(summary = "Criar um novo usuario")
+    //@ApiResponses(value = {
+    //        @ApiResponse(responseCode = "201", description = "Usuário criado com sucesso"),
+    //        @ApiResponse(responseCode = "409", description = "Usuário ou Email já cadastrados no sistema")
+    //})
     public ResponseEntity<Void> criarUsuario(@RequestBody @Valid CriarUsuarioDTO criarUsuarioDTO) {
         log.info("Recebida requisição para criar usuário");
         usuarioUseCase.criarUsuario(criarUsuarioDTO);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
+    //@Operation(summary = "Verificar primeiro acesso")
+    //@ApiResponses(value = {
+    //        @ApiResponse(responseCode = "200", description = "Usuario existe na base de dados e foi validado"),
+    //        @ApiResponse(responseCode = "404", description = "Usuário não existe")
+    //})
     @GetMapping("/primeiroAcesso/{usuario}")
     public ResponseEntity<?> verificarPrimeiroAcesso(@PathVariable String usuario) {
         log.info("Verificando se é o primeiro acesso do usuario: {}", usuario);
